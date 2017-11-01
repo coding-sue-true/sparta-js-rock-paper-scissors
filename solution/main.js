5fe1ab807c307ede93ed8794798d213bf1663002
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2017-11-01 11:01:01 +0000

    homework solution: user input created

---
+++ solution/main.js
@@ -0,0 +1,44 @@
+//get input
+
+function getUserInput() {
+  return prompt('Please choose either "rock", "paper" or "scissors"');
+}
+
+var test = getUserInput();
+
+console.log(test);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// get the player move
+
+
+// Random play
+
+
+//Get the computer move
+
+
+// will need to play to five
+
+
+//check for a winner
