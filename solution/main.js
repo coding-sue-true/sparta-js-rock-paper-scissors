82d1d7225fddb02e2422f0c8ea50c3ae473d06c9
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2017-11-01 11:20:58 +0000

    Homework solution: random input from computer is working

--- solution/main.js
+++ solution/main.js
@@ -4,36 +4,30 @@ function getUserInput() {
   return prompt('Please choose either "rock", "paper" or "scissors"');
 }
 
-var test = getUserInput();
-
-console.log(test);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // get the player move
-
+function getPlayerMove() {
+  return getUserInput();
+}
 
 // Random play
+function randomPlay() {
+  var randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+    return "rock";
+  } else if (randomNumber === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
 
+
+}
+
+
+
+
+var test = randomPlay();
+console.log(test);
 
 //Get the computer move
 
